85d6a3e447cb08d1472972e0dd46bbe9ec772642
Your Name <you@example.com>
2025-04-14 20:24:02 +0300

    end: i finished the task but remembered about the commits only now

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,14 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return new Promise((resolve, reject) => {
+    if (number >= 0) {
+      resolve(number);
+    } else {
+      reject(new Error('Number is negative'));
+    }
+  });
 }
 
 /**
@@ -33,8 +39,8 @@ function getPromise(/* number */) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return source.then(() => 'success').catch(() => 'fail');
 }
 
 /**
@@ -50,8 +56,12 @@ function getPromiseResult(/* source */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.any(promises)
+    .then((result) => result)
+    .catch(() => {
+      throw new Error('all promises were rejected');
+    });
 }
 
 /**
@@ -73,8 +83,18 @@ function getFirstResolvedPromiseResult(/* promises */) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  if (!Array.isArray(promises)) {
+    throw new TypeError('The first argument must be an array of promises');
+  }
+
+  return Promise.race(promises)
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => {
+      throw error;
+    });
 }
 
 /**
@@ -88,8 +108,18 @@ function getFirstPromiseResult(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  if (!Array.isArray(promises)) {
+    throw new TypeError('The first argument must be an array of promoses');
+  }
+
+  return Promise.all(promises)
+    .then((results) => {
+      return results;
+    })
+    .catch((error) => {
+      throw error;
+    });
 }
 
 /**
@@ -104,8 +134,16 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  if (!Array.isArray(promises)) {
+    throw new TypeError('The first argument must be an array of promises');
+  }
+
+  return Promise.allSettled(promises).then((results) => {
+    return results.map((result) => {
+      return result.status === 'fulfilled' ? result.value : null;
+    });
+  });
 }
 
 /**
@@ -126,8 +164,22 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  if (!Array.isArray(promises)) {
+    throw new TypeError('The first argument must be an array of promises');
+  }
+
+  let result = '';
+
+  return promises
+    .reduce((chain, promise) => {
+      return chain.then(() => {
+        return promise.then((resolvedValue) => {
+          result += resolvedValue;
+        });
+      });
+    }, Promise.resolve())
+    .then(() => result);
 }
 
 module.exports = {
